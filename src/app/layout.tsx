6d2673879e6b01dc4d8dd8baf68e54db42314c53
Radium Coders <radiumcoders@gmail.com>
2025-12-08 19:17:28 +0530

    a little of frontend stuff as some i can see are ... username generate and a little more :]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+import { ThemeProvider } from "next-themes";
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
   subsets: ["latin"],
 });
 
@@ -23,11 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${jetbrainsMono.variable} antialiased dar`}>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,81 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+import { RotateCw } from "lucide-react";
+
+//store somw of the nice usernames in array
+const USERCHAN = [
+  "dancing-mango",
+  "flying-penguin",
+  "sleepy-owl",
+  "curious-cat",
+  "brave-lion",
+];
+// key that will be used to store the username in the local storage
+const STORAGE_KEY = "username";
 
 export default function Home() {
+  function generateRandomUsername() {
+    //select a random index form the USERCHAN array :]
+    const random_index = Math.floor(Math.random() * USERCHAN.length);
+    //generate a random username with a random id of length 4
+    const username = `ananymous-${USERCHAN[random_index]}-${nanoid(4)}`;
+  }
+  //store the username in a state
+  const [username, setUsername] = useState("");
+  //allow only 3 refreshes of the username
+  const [refreshed, setRefreshed] = useState(0);
+  useEffect(() => {
+    function main() {
+      //check if the username is already stored in the local storage
+      const stored = localStorage.getItem(STORAGE_KEY);
+      if (stored) {
+        //set the username from the local storage is exists and return :]
+        setUsername(stored);
+        return;
+      }
+    };
+
+    main();
+  });
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <main className="min-h-screen flex justify-center items-center w-full bg-background">
+      <div className="bg-primary/5 border border-primary/10 p-4 gap-5 flex flex-col rounded-xs">
+        <div className="flex flex-col gap-3">
+          <Label className="text-primary/30 font-light">Your Identity</Label>
+          <div className="flex gap-2">
+            <Input
+              className="rounded-xs shadow-2xs"
+              type="text"
+              placeholder={username}
+              disabled={true}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <Button
+              onClick={() => {
+                if (refreshed >= 3) return;
+                setRefreshed(refreshed + 1);
+                console.log("refreshed", refreshed);
+              }}
+              className="rounded-xs bg-transparent text-primary/30 hover:text-primary/90"
+              variant={"outline"}
+            >
+              <RotateCw />
+            </Button>
+          </div>
         </div>
-      </main>
-    </div>
+        <Button className="uppercase rounded-xs shadow-2xl selection:bg-background selection:text-primary">
+          create secure room
+        </Button>
+        {refreshed > 0 && (
+          <p className="text-sm text-primary/50 text-center">
+            {3 - refreshed} Username refresh.
+          </p>
+        )}
+      </div>
+    </main>
   );
 }
